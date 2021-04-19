4e61d65a7027e929eaa2347240d8f605110ae810
gowthamaraj <gowthamaraj360@gmail.com>
2021-04-20 00:34:04 +0530

    complete v1

--- script.py
+++ script.py
@@ -19,12 +19,27 @@ http_get = bytes(query,'utf-8')
 
 data = ''
 with open('vulns.txt','r') as file:
-    vulns = file.read()
+    vulns = file.readlines()
 
+print(vulns)
 try:
     sock.sendall(http_get)
     data = sock.recvfrom(1024)
     data = data[0]
-    print(data)
-except e:
+    headers = data.splitlines()
+    for header in headers:
+        if re.search('Server:', str(header)):
+            print("*****"+header.decode("utf-8")+"*****")
+        else:
+            print(header.decode("utf-8"))
+    for vuln in vulns:
+        if vuln.strip() in str(data.strip(). decode("utf-8")):
+            print('Found server vulnerable! ', vuln)
+            print('Target: '+str(args['target']))
+            print('Port: '+str(args['port']))
+except socket.error:
+    print("Socket Error", socket.errno)
+except Exception as e:
     print(e)
+finally:
+    sock.close()
